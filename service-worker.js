68148630ac3f17905b03dd4cfbca46e4e3b90c14
mmgcreativity <mmgcreativity@gmail.com>
2026-07-28 22:25:41 +0300

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -10,7 +10,7 @@
  * firebase-messaging-service-worker.js dosyası gerekir; bu dosya onun yerini tutmaz.
  */
 
-const SW_VERSION = '2026-07-28-herocredit';
+const SW_VERSION = '2026-07-28-searchleft';
 
 // Yeni sürüm yayınlanınca hemen devreye girsin (bekleyen SW'yi atla).
 self.addEventListener('install', () => {
