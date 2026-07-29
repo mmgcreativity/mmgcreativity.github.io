6744dc382be8b4d8d20c0b2a8b5e1a322d391bcc
mmgcreativity <mmgcreativity@gmail.com>
2026-07-29 14:02:29 +0300

    Grup gunluk panorama (kategori bolumleri); Bugun+Kaydet kaldirildi; ozet kartlar toolbar hizasi; surum bump

--- service-worker.js
+++ service-worker.js
@@ -10,7 +10,7 @@
  * firebase-messaging-service-worker.js dosyası gerekir; bu dosya onun yerini tutmaz.
  */
 
-const SW_VERSION = '2026-07-29-yillik-grup-enter';
+const SW_VERSION = '2026-07-29-gunluk-panorama-grup';
 
 // Yeni sürüm yayınlanınca hemen devreye girsin (bekleyen SW'yi atla).
 self.addEventListener('install', () => {
