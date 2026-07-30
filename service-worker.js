77dca488debf6e9d8c601b411b1ab96329fbfca9
mmgcreativity <mmgcreativity@gmail.com>
2026-07-30 17:51:19 +0300

    Mobil oturum korumasi: SW oto-yenileme kaldirildi

--- service-worker.js
+++ service-worker.js
@@ -12,7 +12,7 @@
  * FCM için AYRI firebase-messaging-service-worker.js gerekir; bu dosya onun yerini tutmaz.
  */
 
-const SW_VERSION = '2026-07-30-network-first-guncel';
+const SW_VERSION = '2026-07-30-oto-yenile-kaldirildi';
 const CACHE = 'mmg-' + SW_VERSION;
 
 self.addEventListener('install', () => {
