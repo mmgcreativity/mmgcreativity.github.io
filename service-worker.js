e9e9bc66ccbf6cc6a4f39236b1a384a5dba49ae9
mmgcreativity <mmgcreativity@gmail.com>
2026-07-30 08:39:49 +0300

    Vade Sapmasi Vade Hesaplari altina alindi + SMS girisi korumasi

--- service-worker.js
+++ service-worker.js
@@ -11,7 +11,7 @@
  * FCM için AYRI bir firebase-messaging-service-worker.js gerekir; bu dosya onun yerini tutmaz.
  */
 
-const SW_VERSION = '2026-07-30-vade-hesaplari-menu';
+const SW_VERSION = '2026-07-30-sms-guard-vade-menu2';
 const CACHE = 'mmg-' + SW_VERSION;
 
 self.addEventListener('install', () => {
