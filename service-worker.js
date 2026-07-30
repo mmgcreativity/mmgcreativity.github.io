c5e6fe212820f5f402a37d8f0d14c2b754d5f89d
mmgcreativity <mmgcreativity@gmail.com>
2026-07-30 12:06:12 +0300

    Kart boylari esit + Hesabim yatay kaydirma kaldirildi

--- service-worker.js
+++ service-worker.js
@@ -11,7 +11,7 @@
  * FCM için AYRI bir firebase-messaging-service-worker.js gerekir; bu dosya onun yerini tutmaz.
  */
 
-const SW_VERSION = '2026-07-30-rotatif-kart-mobil-hero';
+const SW_VERSION = '2026-07-30-kart-esit-hesap-scroll';
 const CACHE = 'mmg-' + SW_VERSION;
 
 self.addEventListener('install', () => {
