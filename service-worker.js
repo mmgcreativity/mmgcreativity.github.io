3ece1ec9cffbae717cab6275588aa5b70660d3bb
mmgcreativity <mmgcreativity@gmail.com>
2026-07-30 14:10:38 +0300

    Talimat IBAN uyari kaldirildi + banka degisince temizle + cari tek satir

--- service-worker.js
+++ service-worker.js
@@ -11,7 +11,7 @@
  * FCM için AYRI bir firebase-messaging-service-worker.js gerekir; bu dosya onun yerini tutmaz.
  */
 
-const SW_VERSION = '2026-07-30-talimat-tazele-cip-hiza';
+const SW_VERSION = '2026-07-30-iban-uyari-banka-degis-carisatir';
 const CACHE = 'mmg-' + SW_VERSION;
 
 self.addEventListener('install', () => {
