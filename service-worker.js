eca719f632c9bf26dbb79a93a222350ed5cc30a7
mmgcreativity <mmgcreativity@gmail.com>
2026-07-30 14:53:05 +0300

    Mobil: aktif firma cubugu tiklayinca firma listesi inline acilir

--- service-worker.js
+++ service-worker.js
@@ -11,7 +11,7 @@
  * FCM için AYRI bir firebase-messaging-service-worker.js gerekir; bu dosya onun yerini tutmaz.
  */
 
-const SW_VERSION = '2026-07-30-mobil-hesap-menu-alt';
+const SW_VERSION = '2026-07-30-mobil-firma-popup-inline';
 const CACHE = 'mmg-' + SW_VERSION;
 
 self.addEventListener('install', () => {
