d178f1e7583c9147dc1fa45544c8edabfe4f0671
mmgcreativity <mmgcreativity@gmail.com>
2026-07-31 06:30:23 +0300

    IBAN siki filtre + sekme home ikonu kaldirildi + firmas index (davet)

--- service-worker.js
+++ service-worker.js
@@ -12,7 +12,7 @@
  * FCM için AYRI firebase-messaging-service-worker.js gerekir; bu dosya onun yerini tutmaz.
  */
 
-const SW_VERSION = '2026-07-31-iban-gonderen-fix';
+const SW_VERSION = '2026-07-31-iban-strict-tabhome';
 const CACHE = 'mmg-' + SW_VERSION;
 
 self.addEventListener('install', () => {
