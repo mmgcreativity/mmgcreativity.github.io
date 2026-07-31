e4b756656185d46095a2743d284b9a772a14bab2
mmgcreativity <mmgcreativity@gmail.com>
2026-07-31 09:31:10 +0300

    Cari Excel sablonu: Tip kaldirildi (tur=aktif sekme), Firma Kodu ilk sutun

--- service-worker.js
+++ service-worker.js
@@ -12,7 +12,7 @@
  * FCM için AYRI firebase-messaging-service-worker.js gerekir; bu dosya onun yerini tutmaz.
  */
 
-const SW_VERSION = '2026-07-31-ozet-sirala-switch';
+const SW_VERSION = '2026-07-31-cari-sablon-tipsiz';
 const CACHE = 'mmg-' + SW_VERSION;
 
 self.addEventListener('install', () => {
