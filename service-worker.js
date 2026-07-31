45607a0c4047d822f3d6e37c9234f8bd87f598f9
mmgcreativity <mmgcreativity@gmail.com>
2026-07-31 21:17:29 +0300

    Talimat: grup disi cari listesinde yazdikca filtreleme (TR karakter duyarsiz)

--- service-worker.js
+++ service-worker.js
@@ -12,7 +12,7 @@
  * FCM için AYRI firebase-messaging-service-worker.js gerekir; bu dosya onun yerini tutmaz.
  */
 
-const SW_VERSION = '2026-07-31-banka-sirali-disket-kaldirildi';
+const SW_VERSION = '2026-07-31-cari-arama-filtre';
 const CACHE = 'mmg-' + SW_VERSION;
 
 self.addEventListener('install', () => {
