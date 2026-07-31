8bd22b839c4c8290caf82b43cc10f558dff42db3
mmgcreativity <mmgcreativity@gmail.com>
2026-07-31 21:20:17 +0300

    Talimat: alici listesinde MUSTERI/TEDARIKCI tur etiketi kaldirildi

--- service-worker.js
+++ service-worker.js
@@ -12,7 +12,7 @@
  * FCM için AYRI firebase-messaging-service-worker.js gerekir; bu dosya onun yerini tutmaz.
  */
 
-const SW_VERSION = '2026-07-31-cari-arama-filtre';
+const SW_VERSION = '2026-07-31-cari-tur-etiketi-kaldirildi';
 const CACHE = 'mmg-' + SW_VERSION;
 
 self.addEventListener('install', () => {
