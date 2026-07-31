0613f263d685085390c04c7a34bcba323f861668
mmgcreativity <mmgcreativity@gmail.com>
2026-07-31 21:48:18 +0300

    VeriGiris: IBAN/musteri/tedarikci/muhatap/personel/banka listelerine arama kutusu

--- service-worker.js
+++ service-worker.js
@@ -12,7 +12,7 @@
  * FCM için AYRI firebase-messaging-service-worker.js gerekir; bu dosya onun yerini tutmaz.
  */
 
-const SW_VERSION = '2026-07-31-onay-penceresi-kalem-firma-sirasi';
+const SW_VERSION = '2026-07-31-liste-arama-kutulari';
 const CACHE = 'mmg-' + SW_VERSION;
 
 self.addEventListener('install', () => {
