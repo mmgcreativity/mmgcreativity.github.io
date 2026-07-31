21be9706d8ad943ea6057779a192a7cfeef1069f
mmgcreativity <mmgcreativity@gmail.com>
2026-07-31 12:03:21 +0300

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -12,7 +12,7 @@
  * FCM için AYRI firebase-messaging-service-worker.js gerekir; bu dosya onun yerini tutmaz.
  */
 
-const SW_VERSION = '2026-07-31-doviz-ondeger-eur';
+const SW_VERSION = '2026-07-31-firma-datalist-ac';
 const CACHE = 'mmg-' + SW_VERSION;
 
 self.addEventListener('install', () => {
