d1b7f67960428295370818baca254cae2ab0ee82
mmgcreativity <mmgcreativity@gmail.com>
2026-07-31 20:52:07 +0300

    Talimat: kisi listesindeki TC, TC Kimlik No kutusuyla ayni fontta (Inter 14px)

--- service-worker.js
+++ service-worker.js
@@ -12,7 +12,7 @@
  * FCM için AYRI firebase-messaging-service-worker.js gerekir; bu dosya onun yerini tutmaz.
  */
 
-const SW_VERSION = '2026-07-31-kayitli-secici-kaldirildi';
+const SW_VERSION = '2026-07-31-tc-font-hiza';
 const CACHE = 'mmg-' + SW_VERSION;
 
 self.addEventListener('install', () => {
