4aba50e610f3f24c693452b64b98ffdfee3b1593
mmgcreativity <mmgcreativity@gmail.com>
2026-07-31 09:48:08 +0300

    Personeller: toplu Excel (FirmaKodu|AdSoyad|TC), Gorev->TC, ikon sade

--- service-worker.js
+++ service-worker.js
@@ -12,7 +12,7 @@
  * FCM için AYRI firebase-messaging-service-worker.js gerekir; bu dosya onun yerini tutmaz.
  */
 
-const SW_VERSION = '2026-07-31-cari-sablon-tipsiz';
+const SW_VERSION = '2026-07-31-personel-tc-excel';
 const CACHE = 'mmg-' + SW_VERSION;
 
 self.addEventListener('install', () => {
