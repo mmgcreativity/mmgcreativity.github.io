05f36eda115c62a8a1bf8fe7bc13633ca6f3a6bd
mmgcreativity <mmgcreativity@gmail.com>
2026-07-31 09:11:03 +0300

    Kullanici Ozeti KY paneline tasindi + siralama; hesap gecisi avatar/scope; firmas CG kurali

--- service-worker.js
+++ service-worker.js
@@ -12,7 +12,7 @@
  * FCM için AYRI firebase-messaging-service-worker.js gerekir; bu dosya onun yerini tutmaz.
  */
 
-const SW_VERSION = '2026-07-31-bekleyen-5is';
+const SW_VERSION = '2026-07-31-ozet-sirala-switch';
 const CACHE = 'mmg-' + SW_VERSION;
 
 self.addEventListener('install', () => {
