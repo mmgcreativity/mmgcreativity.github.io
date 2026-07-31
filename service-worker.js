9afac0b5a436363afe5061c9f8b6411369b1d862
mmgcreativity <mmgcreativity@gmail.com>
2026-07-31 22:04:57 +0300

    KullaniciYonetimi: Firmalarim secicisi ozel dropdown - firma kodu sari

--- service-worker.js
+++ service-worker.js
@@ -12,7 +12,7 @@
  * FCM için AYRI firebase-messaging-service-worker.js gerekir; bu dosya onun yerini tutmaz.
  */
 
-const SW_VERSION = '2026-07-31-firma-kodu-kullanici-yonetimi';
+const SW_VERSION = '2026-07-31-firmalarim-kodu-sari';
 const CACHE = 'mmg-' + SW_VERSION;
 
 self.addEventListener('install', () => {
