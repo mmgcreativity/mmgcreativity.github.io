81b4723cc3a52c1e74b43bd2fc75907366170c00
mmgcreativity <mmgcreativity@gmail.com>
2026-07-31 05:11:30 +0300

    Kullanici paneli: anasayfa ilk acilis, firma kodu zorunlu, kod/isim fix, davet sorgula+maske+durum

--- service-worker.js
+++ service-worker.js
@@ -12,7 +12,7 @@
  * FCM için AYRI firebase-messaging-service-worker.js gerekir; bu dosya onun yerini tutmaz.
  */
 
-const SW_VERSION = '2026-07-31-begeni-kaldirildi-2';
+const SW_VERSION = '2026-07-31-kullanici-paneli';
 const CACHE = 'mmg-' + SW_VERSION;
 
 self.addEventListener('install', () => {
