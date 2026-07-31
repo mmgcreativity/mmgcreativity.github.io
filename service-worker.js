9cd5fa9f1d4f02bcd25277f6cb0e740e39013eb6
mmgcreativity <mmgcreativity@gmail.com>
2026-07-31 23:56:23 +0300

    HazirMetin: gercek firma adi/VKN ornekleri kaldirildi; grup firmalari her sablonda ve kod sirasinda

--- service-worker.js
+++ service-worker.js
@@ -12,7 +12,7 @@
  * FCM için AYRI firebase-messaging-service-worker.js gerekir; bu dosya onun yerini tutmaz.
  */
 
-const SW_VERSION = '2026-07-31-blogger-aktif';
+const SW_VERSION = '2026-07-31-hazirmetin-firma-ve-ornek-temizligi';
 const CACHE = 'mmg-' + SW_VERSION;
 
 self.addEventListener('install', () => {
