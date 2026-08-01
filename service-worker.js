009d81a88736013b86c7f7fa7844fb88c25faf9d
mmgcreativity <mmgcreativity@gmail.com>
2026-08-01 23:05:54 +0300

    Vade Sapma: fatura aciklamasindan parantez kaldirildi

--- service-worker.js
+++ service-worker.js
@@ -12,7 +12,7 @@
  * FCM için AYRI firebase-messaging-service-worker.js gerekir; bu dosya onun yerini tutmaz.
  */
 
-const SW_VERSION = '2026-08-01-kolon-genislik-tutar-limit';
+const SW_VERSION = '2026-08-01-fatura-metni-sade';
 const CACHE = 'mmg-' + SW_VERSION;
 
 self.addEventListener('install', () => {
