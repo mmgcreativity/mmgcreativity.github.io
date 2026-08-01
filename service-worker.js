8087e6eeb2aa466b7ee2d66597cdf172da3518ff
mmgcreativity <mmgcreativity@gmail.com>
2026-08-02 00:57:59 +0300

    Vade Sapma: Excel/PDF/Paylas butonlari, genis yerlesim, tek Sifirla, tarih uzerine yazma

--- service-worker.js
+++ service-worker.js
@@ -12,7 +12,7 @@
  * FCM için AYRI firebase-messaging-service-worker.js gerekir; bu dosya onun yerini tutmaz.
  */
 
-const SW_VERSION = '2026-08-01-sag-ust-sifirla';
+const SW_VERSION = '2026-08-01-genislet-sifirla-tek';
 const CACHE = 'mmg-' + SW_VERSION;
 
 self.addEventListener('install', () => {
