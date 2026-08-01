d8e771c5b16cbb767e37cac361b179553941f391
mmgcreativity <mmgcreativity@gmail.com>
2026-08-01 21:59:46 +0300

    PERF: acilista zorunlu token/sunucu okumasi kaldirildi - once onbellek, dogrulama arka planda

--- service-worker.js
+++ service-worker.js
@@ -12,7 +12,7 @@
  * FCM için AYRI firebase-messaging-service-worker.js gerekir; bu dosya onun yerini tutmaz.
  */
 
-const SW_VERSION = '2026-08-01-sekme-kapat-blur-play-yildiz';
+const SW_VERSION = '2026-08-01-acilis-hizlandirma';
 const CACHE = 'mmg-' + SW_VERSION;
 
 self.addEventListener('install', () => {
