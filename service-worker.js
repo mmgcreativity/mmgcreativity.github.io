3e381de516821808d99d7b66ce1e9397c5220c7a
mmgcreativity <mmgcreativity@gmail.com>
2026-08-02 01:08:19 +0300

    Vade Sapma: rozetler ayni yukseklik ve genislikte, yanindaki yaziyla hizali

--- service-worker.js
+++ service-worker.js
@@ -12,7 +12,7 @@
  * FCM için AYRI firebase-messaging-service-worker.js gerekir; bu dosya onun yerini tutmaz.
  */
 
-const SW_VERSION = '2026-08-01-panel-konum-sabit';
+const SW_VERSION = '2026-08-01-rozet-hiza';
 const CACHE = 'mmg-' + SW_VERSION;
 
 self.addEventListener('install', () => {
