61e4239d85293297fb02fdf667dd8b0261709dd4
mmgcreativity <mmgcreativity@gmail.com>
2026-08-01 23:26:50 +0300

    Tutar ust siniri kaldirildi - 99.999.999 sadece hizalama referansi, daha buyuk tutar girilebilir

--- service-worker.js
+++ service-worker.js
@@ -12,7 +12,7 @@
  * FCM için AYRI firebase-messaging-service-worker.js gerekir; bu dosya onun yerini tutmaz.
  */
 
-const SW_VERSION = '2026-08-01-enter-satir-silme-fix';
+const SW_VERSION = '2026-08-01-tutar-siniri-kaldirildi';
 const CACHE = 'mmg-' + SW_VERSION;
 
 self.addEventListener('install', () => {
