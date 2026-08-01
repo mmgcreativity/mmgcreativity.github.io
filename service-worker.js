c6e2a112174ee9bc8cfd5fd5f2d3cd2523bba8af
mmgcreativity <mmgcreativity@gmail.com>
2026-08-02 00:16:21 +0300

    Vade Sapma: Geri Al kaldirildi, basliklar ortali, Gun/Calisma Sekli daraltildi, Ay Sonu +, takvim ikinci basista kapaniyor

--- service-worker.js
+++ service-worker.js
@@ -12,7 +12,7 @@
  * FCM için AYRI firebase-messaging-service-worker.js gerekir; bu dosya onun yerini tutmaz.
  */
 
-const SW_VERSION = '2026-08-01-secici-zemin-uyum';
+const SW_VERSION = '2026-08-01-takvim-ac-kapa';
 const CACHE = 'mmg-' + SW_VERSION;
 
 self.addEventListener('install', () => {
