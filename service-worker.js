f458917756b4f6f150ece4d4eebe1671cd660fa6
mmgcreativity <mmgcreativity@gmail.com>
2026-08-01 23:55:20 +0300

    Vade Sapma: secici metni sade, zemin panelle uyumlu (siyah degil)

--- service-worker.js
+++ service-worker.js
@@ -12,7 +12,7 @@
  * FCM için AYRI firebase-messaging-service-worker.js gerekir; bu dosya onun yerini tutmaz.
  */
 
-const SW_VERSION = '2026-08-01-secili-satir-hizala';
+const SW_VERSION = '2026-08-01-secici-zemin-uyum';
 const CACHE = 'mmg-' + SW_VERSION;
 
 self.addEventListener('install', () => {
