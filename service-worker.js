a2133d2dff440f3bb0dada0343d74ac453725af1
mmgcreativity <mmgcreativity@gmail.com>
2026-08-02 01:56:46 +0300

    Vade Sapma: kapama ozeti satiri kaldirildi; eslesmede tutar tekrar yaziliyor

--- service-worker.js
+++ service-worker.js
@@ -12,7 +12,7 @@
  * FCM için AYRI firebase-messaging-service-worker.js gerekir; bu dosya onun yerini tutmaz.
  */
 
-const SW_VERSION = '2026-08-01-eslesme-tutari-geri';
+const SW_VERSION = '2026-08-01-kapama-ozeti-kaldirildi';
 const CACHE = 'mmg-' + SW_VERSION;
 
 self.addEventListener('install', () => {
