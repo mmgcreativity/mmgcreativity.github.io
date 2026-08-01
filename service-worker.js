7eba29941b58399204fb3a0f975f77a535c20101
mmgcreativity <mmgcreativity@gmail.com>
2026-08-01 23:31:36 +0300

    Vade Sapma: acilir listeler koyu temaya uyduruldu; Gun kolonu daraltildi (parantez kalkti)

--- service-worker.js
+++ service-worker.js
@@ -12,7 +12,7 @@
  * FCM için AYRI firebase-messaging-service-worker.js gerekir; bu dosya onun yerini tutmaz.
  */
 
-const SW_VERSION = '2026-08-01-tutar-siniri-kaldirildi';
+const SW_VERSION = '2026-08-01-koyu-tema-select';
 const CACHE = 'mmg-' + SW_VERSION;
 
 self.addEventListener('install', () => {
