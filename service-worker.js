c0c152025e10045264f64af2dfe739caf2c5c330
mmgcreativity <mmgcreativity@gmail.com>
2026-08-01 23:47:21 +0300

    Vade Sapma: kapali secici metni listedeki secili satirla ayni gorunumde

--- service-worker.js
+++ service-worker.js
@@ -12,7 +12,7 @@
  * FCM için AYRI firebase-messaging-service-worker.js gerekir; bu dosya onun yerini tutmaz.
  */
 
-const SW_VERSION = '2026-08-01-ozel-acilir-liste';
+const SW_VERSION = '2026-08-01-secili-satir-hizala';
 const CACHE = 'mmg-' + SW_VERSION;
 
 self.addEventListener('install', () => {
