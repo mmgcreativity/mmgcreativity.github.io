287b7dbdc34507f5e241e8850d1d5b69bafc7156
mmgcreativity <mmgcreativity@gmail.com>
2026-08-02 01:21:19 +0300

    Vade Sapma: panel ic bosluklari alindi (dolgu 22-14, ara 8-6, fatura sutunu 564px)

--- service-worker.js
+++ service-worker.js
@@ -12,7 +12,7 @@
  * FCM için AYRI firebase-messaging-service-worker.js gerekir; bu dosya onun yerini tutmaz.
  */
 
-const SW_VERSION = '2026-08-01-fatura-dar-tahsilat-genis';
+const SW_VERSION = '2026-08-01-panel-bosluk-temizligi';
 const CACHE = 'mmg-' + SW_VERSION;
 
 self.addEventListener('install', () => {
