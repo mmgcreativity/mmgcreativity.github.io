568692d7d1ab907f838b46283c347009e8e2fcff
mmgcreativity <mmgcreativity@gmail.com>
2026-08-01 23:08:48 +0300

    Vade Sapma: tahsilat tutari daraltildi, kapama aciklamasi satirin sagina alindi

--- service-worker.js
+++ service-worker.js
@@ -12,7 +12,7 @@
  * FCM için AYRI firebase-messaging-service-worker.js gerekir; bu dosya onun yerini tutmaz.
  */
 
-const SW_VERSION = '2026-08-01-fatura-metni-sade';
+const SW_VERSION = '2026-08-01-tahsilat-sonuc-saga';
 const CACHE = 'mmg-' + SW_VERSION;
 
 self.addEventListener('install', () => {
