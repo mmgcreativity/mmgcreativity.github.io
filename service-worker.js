55b9850fe53a866c4f9fa49ceced0abecf0b3252
mmgcreativity <mmgcreativity@gmail.com>
2026-08-02 15:30:50 +0300

    IBAN formu tek satira indi: 4 alan + Ekle icerik genisligine gore hizalandi

--- service-worker.js
+++ service-worker.js
@@ -12,7 +12,7 @@
  * FCM için AYRI firebase-messaging-service-worker.js gerekir; bu dosya onun yerini tutmaz.
  */
 
-const SW_VERSION = '2026-08-02-iban-satir-duzeltme';
+const SW_VERSION = '2026-08-02-iban-form-tek-satir';
 const CACHE = 'mmg-' + SW_VERSION;
 
 self.addEventListener('install', () => {
