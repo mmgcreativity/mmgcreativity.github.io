e2b084ee5f1eddb0cb35b3483d3deb4b15ed6a37
mmgcreativity <mmgcreativity@gmail.com>
2026-08-02 13:11:52 +0300

    Banka Yonetimi alt menusunden Tanimlar kaldirildi

--- service-worker.js
+++ service-worker.js
@@ -12,7 +12,7 @@
  * FCM için AYRI firebase-messaging-service-worker.js gerekir; bu dosya onun yerini tutmaz.
  */
 
-const SW_VERSION = '2026-08-02-menu-duzeni';
+const SW_VERSION = '2026-08-02-menu-tanimlar-kaldirildi';
 const CACHE = 'mmg-' + SW_VERSION;
 
 self.addEventListener('install', () => {
