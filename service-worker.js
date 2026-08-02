924430698adc60604b68d34bb7bffc16d3204ce3
mmgcreativity <mmgcreativity@gmail.com>
2026-08-02 15:29:17 +0300

    IBAN satiri tek satira indi (6. sutun tasmasi + rozet/para cakismasi + kirpilan IBAN duzeltildi); sutun basliklari, sutun bazli arama ve siralama eklendi

--- service-worker.js
+++ service-worker.js
@@ -12,7 +12,7 @@
  * FCM için AYRI firebase-messaging-service-worker.js gerekir; bu dosya onun yerini tutmaz.
  */
 
-const SW_VERSION = '2026-08-02-cari-siralama-ve-aciklama';
+const SW_VERSION = '2026-08-02-iban-satir-duzeltme';
 const CACHE = 'mmg-' + SW_VERSION;
 
 self.addEventListener('install', () => {
