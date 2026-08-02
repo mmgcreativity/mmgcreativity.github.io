f74bc5bfb3b9f7c6e0ea6ea937efe9bc6ce1f376
mmgcreativity <mmgcreativity@gmail.com>
2026-08-02 14:00:36 +0300

    Ana baslik acilan ekrana gore degisiyor (Banka Limit Risk Durumu vb.)

--- service-worker.js
+++ service-worker.js
@@ -12,7 +12,7 @@
  * FCM için AYRI firebase-messaging-service-worker.js gerekir; bu dosya onun yerini tutmaz.
  */
 
-const SW_VERSION = '2026-08-02-firma-atama-fix';
+const SW_VERSION = '2026-08-02-ekran-basliklari';
 const CACHE = 'mmg-' + SW_VERSION;
 
 self.addEventListener('install', () => {
