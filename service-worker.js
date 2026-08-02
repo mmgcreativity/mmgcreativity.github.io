792c35545c2b18e40e5f04a24e89af81ef0073a0
mmgcreativity <mmgcreativity@gmail.com>
2026-08-02 13:55:47 +0300

    KRITIK FIX: IBAN kaydinda firma secimi yok sayiliyordu; TUM GRUP rozeti; mobil drill-down menu

--- service-worker.js
+++ service-worker.js
@@ -12,7 +12,7 @@
  * FCM için AYRI firebase-messaging-service-worker.js gerekir; bu dosya onun yerini tutmaz.
  */
 
-const SW_VERSION = '2026-08-02-ayri-ekranlar';
+const SW_VERSION = '2026-08-02-firma-atama-fix';
 const CACHE = 'mmg-' + SW_VERSION;
 
 self.addEventListener('install', () => {
