de54130c9fcbf627592f71944071e58e651ab44d
mmgcreativity <mmgcreativity@gmail.com>
2026-08-02 15:45:29 +0300

    Yatirim Portfoyu Nakit Yonetimi menusunde en alta alindi

--- service-worker.js
+++ service-worker.js
@@ -12,7 +12,7 @@
  * FCM için AYRI firebase-messaging-service-worker.js gerekir; bu dosya onun yerini tutmaz.
  */
 
-const SW_VERSION = '2026-08-02-sohbet-sekmeleri';
+const SW_VERSION = '2026-08-02-portfoy-en-alta';
 const CACHE = 'mmg-' + SW_VERSION;
 
 self.addEventListener('install', () => {
