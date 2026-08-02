5aee25de3479b3efd496ace033d1986d253fa082
mmgcreativity <mmgcreativity@gmail.com>
2026-08-02 12:45:44 +0300

    Cek/senet nakit akisa otomatik aktarilir; Gelirler'den cek girisi kaldirildi; Raporlar'daki mukerrer ekranlar silindi

--- service-worker.js
+++ service-worker.js
@@ -12,7 +12,7 @@
  * FCM için AYRI firebase-messaging-service-worker.js gerekir; bu dosya onun yerini tutmaz.
  */
 
-const SW_VERSION = '2026-08-02-referans-kullanici-secici';
+const SW_VERSION = '2026-08-02-cek-otomatik-akis';
 const CACHE = 'mmg-' + SW_VERSION;
 
 self.addEventListener('install', () => {
