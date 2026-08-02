28236c5503351a90804d4b8bc3c862ffb935d4fa
mmgcreativity <mmgcreativity@gmail.com>
2026-08-02 17:35:12 +0300

    IBAN ekrani hizalama: form kutulari liste sutunlariyla ayni izgarada, esit yukseklik/yazi; Tum IBAN'lari Sil filtre satirina alindi; gorunurluk rozetinde yalnizca firma kodu

--- service-worker.js
+++ service-worker.js
@@ -12,7 +12,7 @@
  * FCM için AYRI firebase-messaging-service-worker.js gerekir; bu dosya onun yerini tutmaz.
  */
 
-const SW_VERSION = '2026-08-02-son-rotus';
+const SW_VERSION = '2026-08-02-iban-ekran-hizalama';
 const CACHE = 'mmg-' + SW_VERSION;
 
 self.addEventListener('install', () => {
