c06f889e05a71309790a55af0b6e6c1561a12f86
mmgcreativity <mmgcreativity@gmail.com>
2026-08-02 14:39:42 +0300

    Butceleme + Nakit Yonetimi tek baslikta birlestirildi

--- service-worker.js
+++ service-worker.js
@@ -12,7 +12,7 @@
  * FCM için AYRI firebase-messaging-service-worker.js gerekir; bu dosya onun yerini tutmaz.
  */
 
-const SW_VERSION = '2026-08-02-gelirler-hiz-cek-senet-dbs';
+const SW_VERSION = '2026-08-02-nakit-yonetimi-birlesti';
 const CACHE = 'mmg-' + SW_VERSION;
 
 self.addEventListener('install', () => {
