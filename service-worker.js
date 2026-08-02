0869917756a5e33e72f9a9394c078fe92a1cffcd
mmgcreativity <mmgcreativity@gmail.com>
2026-08-02 14:09:31 +0300

    Hero altindaki ikinci cizgi kaldirildi; Hazir Metin'de firma alani en uste alindi

--- service-worker.js
+++ service-worker.js
@@ -12,7 +12,7 @@
  * FCM için AYRI firebase-messaging-service-worker.js gerekir; bu dosya onun yerini tutmaz.
  */
 
-const SW_VERSION = '2026-08-02-ekran-basliklari';
+const SW_VERSION = '2026-08-02-tabbar-cizgi-firma-sira';
 const CACHE = 'mmg-' + SW_VERSION;
 
 self.addEventListener('install', () => {
