e12b648b2413f71bac5d5b74d7bc0e55307f6281
mmgcreativity <mmgcreativity@gmail.com>
2026-08-02 12:17:14 +0300

    Portfoy -> Yatirim Portfoyu olarak adlandirildi

--- service-worker.js
+++ service-worker.js
@@ -12,7 +12,7 @@
  * FCM için AYRI firebase-messaging-service-worker.js gerekir; bu dosya onun yerini tutmaz.
  */
 
-const SW_VERSION = '2026-08-02-isletme-raporlar';
+const SW_VERSION = '2026-08-02-yatirim-portfoyu';
 const CACHE = 'mmg-' + SW_VERSION;
 
 self.addEventListener('install', () => {
