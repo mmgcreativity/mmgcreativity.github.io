bbd80ab6b636539ed75209ac94c7d1fd0de71503
mmgcreativity <mmgcreativity@gmail.com>
2026-08-02 13:25:54 +0300

    Kart basliklari masaustunde gizlendi (sadece mobil akordiyon); nav_teminat anahtar cakismasi duzeltildi

--- service-worker.js
+++ service-worker.js
@@ -12,7 +12,7 @@
  * FCM için AYRI firebase-messaging-service-worker.js gerekir; bu dosya onun yerini tutmaz.
  */
 
-const SW_VERSION = '2026-08-02-fatura-vade-menu';
+const SW_VERSION = '2026-08-02-teminat-etiket-fix';
 const CACHE = 'mmg-' + SW_VERSION;
 
 self.addEventListener('install', () => {
